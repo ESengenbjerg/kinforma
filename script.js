d7fd4dfa69be1f8fb42859a644abb0c548dcbc99
Daniella Saadon <saadondaniella@gmail.com>
2025-11-26 15:54:25 +0100

    alla senaste uppdateringa

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ cards.forEach((card) => {
     });
   });
 });
+
+const images = [
+  "produkter/Studiobild_alternativ02_jacka.png",
+  "produkter/Studiobild_alternativ01_jacka.png",
+];
+
+let index = 0;
+
+const imgElement = document.getElementById("slideshow");
+
+// Visa första bilden direkt
+imgElement.src = images[index];
+imgElement.style.opacity = 1;
+
+function showImage(newIndex) {
+  imgElement.style.opacity = 0;
+
+  setTimeout(() => {
+    index = (newIndex + images.length) % images.length;
+    imgElement.src = images[index];
+    imgElement.style.opacity = 1;
+  }, 500);
+}
+
+setInterval(() => {
+  showImage(index + 1);
+}, 3000);
